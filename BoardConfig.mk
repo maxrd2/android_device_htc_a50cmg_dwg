43fe4c572cb03b9a0f635faa3bad46ffeda6b049
Mladen Milinkovic <maxrd2@smoothware.net>
2019-06-14 13:35:41 +0200

    initial commit

---
+++ BoardConfig.mk
@@ -0,0 +1,52 @@
+DEVICE_TREE := device/htc/a50cmg_dwg
+
+# Bootloader
+#TARGET_NO_BOOTLOADER := true
+
+# Platform
+TARGET_BOARD_PLATFORM := mt6753
+TARGET_BOOTLOADER_BOARD_NAME := htc_a50cmg_dwg
+
+# Architecture
+TARGET_ARCH := arm64
+TARGET_ARCH_VARIANT := armv8-a
+TARGET_CPU_ABI := arm64-v8a
+TARGET_CPU_ABI2 :=
+TARGET_CPU_VARIANT := generic
+TARGET_CPU_SMP := true
+
+TARGET_2ND_ARCH := arm
+TARGET_2ND_ARCH_VARIANT := armv7-a-neon
+TARGET_2ND_CPU_ABI := armeabi-v7a
+TARGET_2ND_CPU_ABI2 := armeabi
+TARGET_2ND_CPU_VARIANT := generic
+
+# Kernel
+BOARD_KERNEL_CMDLINE := bootopt=64S3,32N2,64N2 androidusb.pid=0x05E1 androidboot.hardware=mt6753 androidboot.mod1=normal androidboot.selinux=permissive
+BOARD_KERNEL_BASE := 0x40078000
+BOARD_KERNEL_PAGESIZE := 2048
+BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x03f88000 --tags_offset 0x0df88000
+TARGET_PREBUILT_KERNEL := $(DEVICE_TREE)/kernel
+
+# Partitions
+TARGET_USERIMAGES_USE_EXT4 := true
+TARGET_USERIMAGES_USE_F2FS := true
+
+# Recovery
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 14112768
+BOARD_FLASH_BLOCK_SIZE := 0
+BOARD_HAS_NO_SELECT_BUTTON := true
+BOARD_HAS_NO_REAL_SDCARD := true
+BOARD_HAS_NO_MISC_PARTITION := true
+BOARD_RECOVERY_SWIPE := true
+BOARD_SUPPRESS_SECURE_ERASE := true
+BOARD_USES_MMCUTILS := true
+BOARD_SUPPRESS_EMMC_WIPE := true
+TW_INPUT_BLACKLIST := "hbtp_vm"
+#TARGET_RECOVERY_DEVICE_MODULES := chargeled
+TW_CUSTOM_CPU_TEMP_PATH := /sys/class/thermal/thermal_zone1/temp
+
+# TWRP
+BOARD_USE_CUSTOM_RECOVERY_FONT := \"roboto_23x41.h\"
+TW_THEME := portrait_hdpi
+TW_HAS_DOWNLOAD_MODE := true
---
+++ README.md
@@ -0,0 +1,25 @@
+## TWRP device tree for HTC Desire 728G Dual Sim (a50cmg_dwg)
+
+For building TWRP for MT6753 models only.
+
+
+### Manifest
+Add to `.repo/local_manifests/a50cmg_dwg.xml`:
+
+```xml
+<?xml version="1.0" encoding="UTF-8"?>
+<manifest>
+	<project name="maxrd2/android_device_htc_a50cmg_dwg" path="device/htc/a50cmg_dwg" remote="github" revision="android-5.1" />
+</manifest>
+```
+
+Then run `repo sync` to check it out.
+
+### Build
+To build:
+
+```sh
+. build/envsetup.sh
+lunch omni_a50cmg_dwg-eng
+make -j4 recoveryimage
+```
